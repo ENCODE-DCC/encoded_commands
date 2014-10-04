84edb9ab46cbb42f9979d214051c596c11dd020f
Laurence Rowe <lrowe@stanford.edu>
2014-10-03 18:51:00 -0700

    encoded commands needed on mahout

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='encoded_commands',
+    version='0.1',
+    description='',
+    packages=['encoded_commands'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points='''
+        [console_scripts]
+        check-files = encoded_commands.check_files:main
+        update-file-status = encoded_commands.update_file_status:main
+        ''',
+)
+
