097f46dbdf10ae54d1584c7fc97bb95c5680cb22
Laurence Rowe <laurence@lrowe.co.uk>
2015-02-27 13:09:46 -0800

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='encoded_commands',
-    version='0.5',
+    version='0.6',
     description='',
     packages=['encoded_commands'],
     include_package_data=True,
